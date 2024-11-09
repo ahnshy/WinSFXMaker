82fa60c09470fb34af24638a8de08c1d9525c8f6
ahnshy <ahnshy@naver.com>
2024-11-09 22:01:42 +0900

    <feat>: Add execution assembly parser function implementation

--- SFXTemplete/SFXTemplete.cpp
+++ SFXTemplete/SFXTemplete.cpp
@@ -100,3 +100,54 @@ BOOL CSFXTempleteApp::ExtractEmbeddedZip(LPCTSTR outputPath)
 	return TRUE;
 }
 
+INT32 CSFXTempleteApp::Parse(LPTSTR m_lpCmdLine)
+{
+	if (m_lpCmdLine == NULL || m_lpCmdLine[0] == NULL)
+		return 0;
+
+	{
+		TCHAR seps[] = _T(" ");
+		TCHAR* pToken = NULL;
+		TCHAR* p = NULL;
+		pToken = _tcstok(m_lpCmdLine, seps); // establish first token            
+		while (pToken != NULL)
+		{
+			do
+			{
+				if (((p = _tcsstr(wcsupr(pToken), _T("/w"))) != NULL) || ((p = _tcsstr(wcsupr(pToken), _T("/W"))) != NULL))
+				{
+
+					//m_strPath = _tcstok(NULL, _T("\""));
+					//PathAddBackslash(m_strPath.GetBuffer(512));
+					//m_strPath.ReleaseBuffer();
+					break;
+				}
+
+				if (((p = _tcsstr(wcsupr(pToken), _T("/p"))) != NULL) || ((p = _tcsstr(wcsupr(pToken), _T("/P"))) != NULL))
+				{
+					//m_strPatientID = _tcstok(NULL, _T("\""));
+					break;
+				}
+
+				if (((p = _tcsstr(wcsupr(pToken), _T("/n"))) != NULL) || ((p = _tcsstr(wcsupr(pToken), _T("/N"))) != NULL))
+				{
+					//m_strName = _tcstok(NULL, _T("\""));
+					break;
+				}
+
+				if (((p = _tcsstr(wcsupr(pToken), _T("/r"))) != NULL) || ((p = _tcsstr(wcsupr(pToken), _T("/R"))) != NULL))
+				{
+					//m_strRecordID = _tcstok(NULL, _T("\""));
+					break;
+				}
+
+			} while (0);
+			pToken = _tcstok(NULL, seps);
+		}
+	}
+
+	//if (m_strPath.IsEmpty() || m_strPatientID.IsEmpty() || m_strName.IsEmpty() || m_strRecordID.IsEmpty())
+	//	return 0;
+
+	return 1;
+}
