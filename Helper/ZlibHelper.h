0261c86701b9c4b567732ebd33441c5daf4779ad
ahnshy <ahnshy@naver.com>
2024-11-09 20:49:07 +0900

    <feat> : add zlib helper header

--- Helper/ZlibHelper.h
+++ Helper/ZlibHelper.h
@@ -5,6 +5,9 @@ public:
 	CZlibHelper();
 	~CZlibHelper();
 
+
+	BOOL Compress(LPCTSTR inputFilePath, LPCTSTR outputFilePath);
+	BOOL Decompress(LPCTSTR inputFilePath, LPCTSTR outputFilePath);
 	//BOOL Decompress(LPCTSTR inputFilePath, LPCTSTR outputFilePath);
 };
 
