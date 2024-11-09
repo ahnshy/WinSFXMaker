3b287c61cf61adcfcef6d94eddb9e9158785dcb1
ahnshy <ahnshy@naver.com>
2024-11-09 19:32:35 +0900

    <feat> : Add zlib helper class

---
+++ Helper/ZlibHelper.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "ZlibHelper.h"
+
+
+CZlibHelper::CZlibHelper()
+{
+}
+
+
+CZlibHelper::~CZlibHelper()
+{
+}
---
+++ Helper/ZlibHelper.h
@@ -0,0 +1,8 @@
+#pragma once
+class CZlibHelper
+{
+public:
+	CZlibHelper();
+	~CZlibHelper();
+};
+
