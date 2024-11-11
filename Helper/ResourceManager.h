42606ce90cde4f4f77a63b93ed3152143d313cf1
ahnshy <ahnshy@naver.com>
2024-11-11 20:39:50 +0900

    <refactor> : move to method UpdateVersionInfo(LPCTSTR szExePath, const CString& productName, const CString& fileVersion, const CString& companyName)

--- Helper/ResourceManager.h
+++ Helper/ResourceManager.h
@@ -7,6 +7,8 @@ public:
 	~CResourceManager();
 
 	BOOL ChangeExeIcon(LPCTSTR szExePath, LPCTSTR szIconFilePath);
+	
 	BOOL GetVersionInfo(LPCTSTR szExePath, CString& fileVersion, CString& productName, CString& companyName);
+	BOOL UpdateVersionInfo(LPCTSTR szExePath, const CString& productName, const CString& fileVersion, const CString& companyName);
 };
 
