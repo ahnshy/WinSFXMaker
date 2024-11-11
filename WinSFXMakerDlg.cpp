3219c05017933b2e1d4c138903fbb82462373d0f
ahnshy <ahnshy@naver.com>
2024-11-11 20:31:06 +0900

    <refactor> : move to method ChangeExeIcon(LPCTSTR szExePath, LPCTSTR szIconFilePath)

--- WinSFXMakerDlg.cpp
+++ WinSFXMakerDlg.cpp
@@ -258,37 +258,6 @@ BOOL CWinSFXMakerDlg::CreateSFXFile(LPCTSTR templatePath, LPCTSTR zipPath, LPCTS
 	return TRUE;
 }
 
-BOOL CWinSFXMakerDlg::ChangeExeIcon(LPCTSTR szExePath, LPCTSTR szIconFilePath)
-{
-	// EXE 파일 열기
-	HANDLE hUpdate = BeginUpdateResource(szExePath, FALSE);
-	if (hUpdate == NULL) {
-		//MessageBox(NULL, _T("EXE 파일을 열 수 없습니다."), _T("Error"), MB_OK);
-		return FALSE;
-	}
-
-	// 아이콘 리소스 변경
-	HICON hIcon = (HICON)LoadImage(NULL, szIconFilePath, IMAGE_ICON, 0, 0, LR_LOADFROMFILE);
-	if (hIcon == NULL) {
-		//MessageBox(NULL, _T("아이콘 파일을 로드할 수 없습니다."), _T("Error"), MB_OK);
-		EndUpdateResource(hUpdate, TRUE);
-		return FALSE;
-	}
-
-	// 리소스 업데이트
-	BOOL result = UpdateResource(hUpdate, RT_ICON, MAKEINTRESOURCE(101), MAKELANGID(LANG_NEUTRAL, SUBLANG_NEUTRAL), hIcon, sizeof(HICON));
-
-	if (!result) {
-		//MessageBox(NULL, _T("리소스를 업데이트할 수 없습니다."), _T("Error"), MB_OK);
-		EndUpdateResource(hUpdate, TRUE);
-		return FALSE;
-	}
-
-	// 리소스 업데이트 완료
-	EndUpdateResource(hUpdate, FALSE);
-	return TRUE;
-}
-
 BOOL CWinSFXMakerDlg::UpdateVersionInfo(LPCTSTR szExePath, const CString& productName, const CString& fileVersion, const CString& companyName)
 {
 	// EXE 파일 열기
