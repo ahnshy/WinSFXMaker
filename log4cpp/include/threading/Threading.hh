c50dcbf71f0226d843bb77410c696c356e4c2670
ahnshy@naver.com <ahnshy@naver.com>
2024-11-13 07:09:55 +0900

    <fix>: Modifying the path of the header file according to the customization of the log4cpp library

--- log4cpp/include/threading/Threading.hh
+++ log4cpp/include/threading/Threading.hh
@@ -10,28 +10,28 @@
 #ifndef _LOG4CPP_THREADING_THREADING_HH
 #define _LOG4CPP_THREADING_THREADING_HH
 
-#include <log4cpp/Portability.hh>
+#include <../include/Portability.hh>
 
 #ifdef LOG4CPP_HAVE_THREADING
 
 #ifdef LOG4CPP_USE_OMNITHREADS
-#include <log4cpp/threading/OmniThreads.hh>
+#include <../include/threading/OmniThreads.hh>
 #endif
 
 #ifdef LOG4CPP_USE_BOOSTTHREADS
-#include <log4cpp/threading/BoostThreads.hh>
+#include <../include/threading/BoostThreads.hh>
 #endif
 
 #ifdef LOG4CPP_USE_MSTHREADS
-#include <log4cpp/threading/MSThreads.hh>
+#include <../include/threading/MSThreads.hh>
 #endif
 
 #ifdef LOG4CPP_USE_PTHREADS
-#include <log4cpp/threading/PThreads.hh>
+#include <../include/threading/PThreads.hh>
 #endif
 
 #else /* LOG4CPP_HAVE_THREADING */
-#include <log4cpp/threading/DummyThreads.hh>
+#include <../include/threading/DummyThreads.hh>
 #endif /* LOG4CPP_HAVE_THREADING */
 
 #endif
