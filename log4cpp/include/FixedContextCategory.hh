910f4f6718aa08b789588778b8ef44dce86eeea9
ahnshy@naver.com <ahnshy@naver.com>
2024-11-13 07:02:00 +0900

    <fix>: Modifying the path of the header file according to the customization of the log4cpp library

--- log4cpp/include/FixedContextCategory.hh
+++ log4cpp/include/FixedContextCategory.hh
@@ -10,8 +10,8 @@
 #ifndef _LOG4CPP_FIXEDCONTEXTCATEGORY_HH
 #define _LOG4CPP_FIXEDCONTEXTCATEGORY_HH
 
-#include <log4cpp/Portability.hh>
-#include <log4cpp/Category.hh>
+#include <../include/Portability.hh>
+#include <../include/Category.hh>
 
 namespace log4cpp {
 
