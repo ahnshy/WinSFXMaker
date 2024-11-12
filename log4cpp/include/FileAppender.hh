089adecce85bba63e8f4d706241c0cfbfadbf656
ahnshy@naver.com <ahnshy@naver.com>
2024-11-13 07:01:47 +0900

    <fix>: Modifying the path of the header file according to the customization of the log4cpp library

--- log4cpp/include/FileAppender.hh
+++ log4cpp/include/FileAppender.hh
@@ -10,8 +10,8 @@
 #ifndef _LOG4CPP_FILEAPPENDER_HH
 #define _LOG4CPP_FILEAPPENDER_HH
 
-#include <log4cpp/Portability.hh>
-#include <log4cpp/LayoutAppender.hh>
+#include <../include/Portability.hh>
+#include <../include/LayoutAppender.hh>
 #include <string>
 #include <stdarg.h>
 
