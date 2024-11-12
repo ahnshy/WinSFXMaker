23f9cdfe8447ece9caf7ba8226348885bfc42188
ahnshy@naver.com <ahnshy@naver.com>
2024-11-13 07:02:16 +0900

    <fix>: Modifying the path of the header file according to the customization of the log4cpp library

--- log4cpp/include/Layout.hh
+++ log4cpp/include/Layout.hh
@@ -10,8 +10,8 @@
 #ifndef _LOG4CPP_LAYOUT_HH
 #define _LOG4CPP_LAYOUT_HH
 
-#include <log4cpp/Portability.hh>
-#include <log4cpp/LoggingEvent.hh>
+#include <../include/Portability.hh>
+#include <../include/LoggingEvent.hh>
 #include <string>
 
 namespace log4cpp {
