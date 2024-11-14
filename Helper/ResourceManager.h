ffd34058bf484a584e4ca0d1d2fe1ffe0774a7d9
ahnshy <ahnshy@naver.com>
2024-11-14 20:32:20 +0900

    <refactor>: Match variable name with Hungarian naming

--- Helper/ResourceManager.h
+++ Helper/ResourceManager.h
@@ -8,7 +8,7 @@ public:
 
 	BOOL ChangeExeIcon(LPCTSTR lpszExePath, LPCTSTR lpszIconFilePath);
 	
-	BOOL GetVersionInfo(LPCTSTR szExePath, CString& fileVersion, CString& productName, CString& companyName);
+	BOOL GetVersionInfo(LPCTSTR lpszExePath, CString& strFileVersion, CString& strProductName, CString& strCompanyName);
 	BOOL UpdateVersionInfo(LPCTSTR szExePath, const CString& productName, const CString& fileVersion, const CString& companyName);
 };
 
