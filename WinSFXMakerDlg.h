7811f95699f7e4bd861e82afc175d6911a40342c
ahnshy <ahnshy@naver.com>
2024-11-17 16:37:44 +0900

    <feat> : OnWindowPosChanged override declaration for main dialog window resizing processing

--- WinSFXMakerDlg.h
+++ WinSFXMakerDlg.h
@@ -41,6 +41,7 @@ protected:
 	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
 	afx_msg void OnPaint();
 	afx_msg HCURSOR OnQueryDragIcon();
+	afx_msg					void OnWindowPosChanged(WINDOWPOS* lpwndpos);
 	DECLARE_MESSAGE_MAP()
 public:
 	afx_msg void OnBnClickedOk();
