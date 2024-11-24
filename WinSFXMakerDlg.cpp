6a75ab41125f79a17ab3fb9edfe0d381e7f84142
ahnshy <ahnshy@naver.com>
2024-11-24 15:06:23 +0900

    <feat>: Remove unnecessary method calls for m_btnDirectory member variables that open folders

--- WinSFXMakerDlg.cpp
+++ WinSFXMakerDlg.cpp
@@ -329,8 +329,6 @@ void CWinSFXMakerDlg::OnBnClickedOk()
 void CWinSFXMakerDlg::Initialize()
 {
 	m_btnDirectory.SetImage(IDB_BITMAP_DIRECTORY, IDB_BITMAP_DIRECTORY_HOT);
-	//m_btnDirectory.SizeToContent();
-	m_btnDirectory.SetMouseCursorHand();
 	m_btnDirectory.SetWindowText(_T(""));
 	m_btnDirectory.Invalidate();
 
