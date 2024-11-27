59292320d83b34d14c1f467dbef5b5460a87c6b1
ahnshy <ahnshy@naver.com>
2024-11-27 20:39:53 +0900

    <refactor> : Change from bool to BOOL type for return type

--- Helper/ShellZipHelper.h
+++ Helper/ShellZipHelper.h
@@ -10,15 +10,15 @@ public:
 	~CShellZipHelper();
 
 	// ZIP 파일 생성 (압축)
-	bool CreateZip(const CString& zipFilePath, const CStringArray& filesToCompress);
+	BOOL CreateZip(const CString& zipFilePath, const CStringArray& filesToCompress);
 
 	// ZIP 파일에서 압축 해제
-	bool ExtractZip(const CString& zipFilePath, const CString& extractToFolder);
+	BOOL ExtractZip(const CString& zipFilePath, const CString& extractToFolder);
 
 private:
 	// 빈 ZIP 파일 생성
-	bool CreateEmptyZip(const CString& zipFilePath);
+	BOOL CreateEmptyZip(const CString& zipFilePath);
 
 	// 파일을 ZIP에 추가
-	bool AddFileToZip(CComPtr<Folder>& pZipFolder, const CString& filePath);
+	BOOL AddFileToZip(CComPtr<Folder>& pZipFolder, const CString& filePath);
 };
