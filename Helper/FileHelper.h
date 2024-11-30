525ad361fc54bf2f698d8c345253e0c0df68839a
ahnshy <ahnshy@naver.com>
2024-11-30 20:16:51 +0900

    <feat> : Definition a FileHelper class

---
+++ Helper/FileHelper.h
@@ -0,0 +1,20 @@
+#pragma once
+
+/////////////////////////////////////////////////////////////////////////
+// #20120118 Work Ahnshy :: File Utility
+// CFileHelper class
+// Bug Report :: ahnshy@naver.com
+
+class CFileHelper
+{
+public:
+	static	CString			GetGUID();
+	static	void			CreateRandomString(TCHAR* pszBuf, int nLen);
+	static	void			GetTimeBaseRandomString(TCHAR* pszBuf, int nNumOfChar);
+	static	CString			GetTimeBaseFileName(LPCTSTR lpBasePath, LPCTSTR lpExt = _T("tmp"));
+	static	CString			GetGuidBaseFileName(CString strPath, CString strExt);
+	static	BOOL			IsSupportMedia(CString strPath);
+	static	BOOL			IsSupportImage(CString strPath);
+	static	BOOL			IsSupportThumbnail(LPCTSTR lpExt);
+	static	BOOL			ShowFileDialog(HWND hOwner, IN LPTSTR lpszFilter, IN LPCTSTR lpInitDir, IN OUT LPTSTR pszFilePath, IN LPCTSTR lpszTitle, IN LPCTSTR lpDefaultExt, IN int& nFilterIndex);
+};
