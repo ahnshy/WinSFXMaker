a7497d0e0c570ce99ad297b266c53a148cc3d3b7
ahnshy <ahnshy@naver.com>
2024-12-01 14:40:36 +0900

    <feat> : Append DlgPrgress.cpp

---
+++ Dialog/DlgPrgress.cpp
@@ -0,0 +1,152 @@
+// DlgPrgress.cpp : implementation file
+//
+
+#include "stdafx.h"
+
+#include <afxdialogex.h>
+#include "DlgPrgress.h"
+#include "../Narcissus.h"
+
+#include "afxdatarecovery.h"
+#include "afxglobals.h"
+#include "afxwinappex.h"
+#include "afxdialogex.h"
+
+
+// CDlgPrgress dialog
+
+IMPLEMENT_DYNAMIC(CDlgPrgress, CDialogEx)
+
+CDlgPrgress::CDlgPrgress(CWnd* pParent /*=NULL*/)
+	: CDialogEx(CDlgPrgress::IDD, pParent)
+{
+	m_dwMax			= 0;
+	m_pTaskbarList3 = NULL;
+	::CoInitialize(NULL);
+	CoCreateInstance(CLSID_TaskbarList, NULL, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&m_pTaskbarList3));
+}
+
+CDlgPrgress::~CDlgPrgress()
+{
+	if (m_pTaskbarList3)
+	{
+		m_pTaskbarList3->Release();
+		m_pTaskbarList3 = NULL;
+	}
+	::CoUninitialize();
+}
+
+void CDlgPrgress::DoDataExchange(CDataExchange* pDX)
+{
+	DDX_Control(pDX, IDC_PROGRESS, m_wndProgress);
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CDlgPrgress, CDialogEx)
+END_MESSAGE_MAP()
+
+BOOL CDlgPrgress::OnInitDialog()
+{
+	SetBackgroundColor(RGB(50, 50, 50));
+
+	LONG ExtendedStyle = GetWindowLong(GetSafeHwnd(),GWL_EXSTYLE);
+	SetWindowLong(GetSafeHwnd(),GWL_EXSTYLE, ExtendedStyle | WS_EX_LAYERED);
+	::SetLayeredWindowAttributes(GetSafeHwnd(), 0, 150, LWA_ALPHA);
+
+	UpdateData(FALSE);
+
+	CRect rt;
+	GetClientRect(&rt);
+
+	m_wndProgress.SetWindowPos(NULL, rt.left + 10, rt.top + (rt.Height()/2 - 50), rt.Width() - 17,  20, SWP_SHOWWINDOW);
+
+	return TRUE;
+}
+
+void CDlgPrgress::InitProgessRange(DWORD dwMax, DWORD dwMin)
+{
+	m_dwMax = dwMax;
+	if (!m_wndProgress.GetSafeHwnd())
+		return;
+	
+	m_wndProgress.SetRange((short)dwMin, (short)dwMax);
+	m_wndProgress.SetStep(1);
+
+	InitTaskProgress();
+}
+
+void CDlgPrgress::SetProgressStep(DWORD dwStep)
+{
+	if (!m_wndProgress.GetSafeHwnd())
+		return;
+
+	m_wndProgress.SetStep(dwStep);
+	SetTaskProgressPos(dwStep);
+}
+
+void CDlgPrgress::ProgressStepIt()
+{
+	if (!m_wndProgress.GetSafeHwnd())
+		return;
+
+	m_wndProgress.StepIt();
+
+	SetTaskProgressPos(m_wndProgress.GetPos());
+}
+
+void CDlgPrgress::UpdateText(UINT uID)
+{
+	TCHAR szBuffer[BUFSIZ] = { NULL, };
+	::LoadString(NULL, uID, szBuffer, BUFSIZ);
+	UpdateText(szBuffer);
+}
+
+void CDlgPrgress::UpdateText(LPCTSTR lpszText)
+{
+	CWnd *pWnd = GetDlgItem(IDC_STATIC);
+	if (!pWnd || !lpszText)
+		return;
+
+	CDC* pDC = pWnd->GetWindowDC();
+	CRect rt;
+	pWnd->GetWindowRect(&rt);
+	pDC->SetTextColor(RGB(255, 0, 0));
+	pDC->DrawText(lpszText, _tcslen(lpszText), &rt, DT_CENTER | DT_SINGLELINE | DT_VCENTER);
+}
+
+void CDlgPrgress::InitTaskProgress()
+{
+	if (m_pTaskbarList3)
+		m_pTaskbarList3->HrInit();
+
+	SetTaskProgressStatus(TBPF_NORMAL);
+}
+
+void CDlgPrgress::SetTaskProgressPos(int nProgressPos)
+{
+	if (!afxGlobalData.bIsWindows7 || !m_pTaskbarList3)
+		return;
+
+	m_pTaskbarList3->SetProgressValue(GetParent()->GetSafeHwnd(), nProgressPos, m_dwMax);
+}
+void CDlgPrgress::SetTaskProgressStatus(TBPFLAG tbpFlags)
+{
+	if (!afxGlobalData.bIsWindows7 || !m_pTaskbarList3)
+		return;
+
+	m_pTaskbarList3->SetProgressState(GetParent()->GetSafeHwnd(), tbpFlags);
+}
+
+void CDlgPrgress::CleanTaskProgress()
+{
+	SetTaskProgressStatus(TBPF_NOPROGRESS);
+}
+
+BOOL CDlgPrgress::PreTranslateMessage(MSG* pMsg)
+{
+	if ((pMsg->message == WM_KEYDOWN && pMsg->wParam == VK_ESCAPE) ||
+		(pMsg->message == WM_KEYDOWN && pMsg->wParam == VK_RETURN))
+		return TRUE;
+
+	return CDialogEx::PreTranslateMessage(pMsg);
+}
