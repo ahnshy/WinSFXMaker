634f0ed212208cf3aec15da44cbf6b39f87052f2
ahnshy@naver.com <ahnshy@naver.com>
2024-12-03 07:28:29 +0900

    <fix>: Remove unnecessary methods

--- SFXTemplete/Helper/ZipHelper.h
+++ SFXTemplete/Helper/ZipHelper.h
@@ -12,12 +12,7 @@ public:
 
 public:
 	BOOL Decompress(CString strDecompressTarget, CString strDestPath);
-	BOOL Compress(CString strOutputName);
-	BOOL AddCompressTarget(CString strTargetPath);
 
 private:
-	BOOL IsTargetExist(LPCTSTR lpszTarget);
-	CStringArray m_arrTarget;
-
 	CZipArchive m_zip;
 };
