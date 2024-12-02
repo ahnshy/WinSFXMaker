917b65ab74c5b4e85d81a2f384fb8d4adc58decc
ahnshy@naver.com <ahnshy@naver.com>
2024-12-03 07:07:17 +0900

    <feat> : Add Zip Helper class for decompression in SFXTemplete

---
+++ SFXTemplete/Helper/ZipHelper.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "../ZipArchive/ZipArchive.h"
+/////////////////////////////////////////////////////////////////////////////
+// CZip
+
+class CZipHelper
+{
+public:
+	CZipHelper();
+	~CZipHelper();
+
+public:
+	BOOL Decompress(CString strDecompressTarget, CString strDestPath);
+	BOOL Compress(CString strOutputName);
+	BOOL AddCompressTarget(CString strTargetPath);
+
+private:
+	BOOL IsTargetExist(LPCTSTR lpszTarget);
+	CStringArray m_arrTarget;
+
+	CZipArchive m_zip;
+};
