f0b263ce8a6cbd847dc6c76f74c390f488e8dfbb
ahnshy@naver.com <ahnshy@naver.com>
2024-12-03 07:21:33 +0900

    <fix>: Inlude zip lib path <fix>: Zip maximum compression size 100MB => 1G improvement

--- SFXTemplete/Helper/ZipHelper.cpp
+++ SFXTemplete/Helper/ZipHelper.cpp
@@ -2,9 +2,8 @@
 //#include "CompressLib.h"
 #include "ZipHelper.h"
 #include <shlwapi.h>
-#include "FileFinderHelper.h"
 
-#pragma comment(lib, "lib\\ZipArchive.lib")
+#pragma comment(lib, "..\\lib\\ZipArchive.lib")
 
 const long	MAX_VOLUME_SIZE_BYTES	= 100000000;
 const int	ZIP_COMPRESS_LEVEL		= 9;
@@ -25,94 +24,6 @@ BOOL CZipHelper::AddCompressTarget(CString strTargetPath)
 		return TRUE;
 
 	m_arrTarget.Add(strTargetPath);
-
-	return TRUE;
-}
-
-BOOL CZipHelper::Compress(CString strOutputName)
-{
-	long lVolumeSize = MAX_VOLUME_SIZE_BYTES;
-	int nLevel = ZIP_COMPRESS_LEVEL;
-
-	TCHAR szOldCurrentDir[2048] = { 0 };
-	GetCurrentDirectory(2048, szOldCurrentDir);
-
-	strOutputName.MakeLower();
-	if(strOutputName.Find(_T(".zip")) < 0)
-	{
-		strOutputName += _T(".zip");
-	}
-
-	try
-	{
-		m_zip.Open((LPCTSTR)strOutputName, CZipArchive::zipCreate, lVolumeSize);
-	}
-	catch(CException* pEx)
-	{
-		pEx->Delete();
-		return FALSE;
-	}
-
-	int nCount = (int)m_arrTarget.GetSize();
-	if(nCount < 0)
-	{
-		m_zip.CloseFile();
-		return FALSE;
-	}
-
-	try
-	{
-		for(int i = 0; i < nCount; i++)
-		{
-			CString strPath = m_arrTarget.GetAt(i);
-			//m_zip.AddNewFile((LPCTSTR)strPath, nLevel);
-			if(::PathIsDirectory((LPCTSTR)strPath))
-			{
-				CFileFinderHelper finder;
-				finder.m_fileTypeToSearch.push_back(_T("*.*"));
-				int nCount = finder.Search(strPath, TRUE);
-
-				if(strPath.Right(1) != _T("\\"))
-				{
-					strPath += _T("\\");
-				}
-
-				::SetCurrentDirectory((LPCTSTR)strPath);
-
-				for(int j = 0; j < (int)finder.m_fileList.size(); j++)
-				{
-					CString strFileName(finder.m_fileList[j].fullpath);
-					strFileName.Replace(strPath, _T(""));
-					m_zip.AddNewFile(strFileName, nCount);
-				}
-			}
-			else
-			{
-				TCHAR szDrive[12];
-				TCHAR szDirectory[MAX_PATH];
-				TCHAR szPath[MAX_PATH];
-				_tsplitpath_s((LPCTSTR)strPath, szDrive, sizeof(szDrive)/sizeof(TCHAR), szDirectory, sizeof(szDirectory)/sizeof(TCHAR), NULL, 0, NULL, 0);
-
-				_tcscpy_s(szPath, sizeof(szPath)/sizeof(TCHAR), szDrive);
-				_tcscat_s(szPath, sizeof(szPath)/sizeof(TCHAR), szDirectory);
-
-				::SetCurrentDirectory(szPath);
-				strPath.Replace(szPath, _T(""));				
-				m_zip.AddNewFile(strPath, nLevel);				
-			}
-		}
-	}
-	catch(CException* pEx2)
-	{
-		m_zip.Close();
-		pEx2->Delete();
-		return FALSE;
-	}
-
-	m_arrTarget.RemoveAll();
-	m_zip.Close();
-	
-	SetCurrentDirectory(szOldCurrentDir);
 
 	return TRUE;
 }
